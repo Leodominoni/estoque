486c6229419702cd4cb4161957b9b28f8bba63f1
Leonor <ldominoni@gmail.com>
2023-05-28 17:45:59 -0300

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductsFormComponent } from './products-form/products-form.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductsFormComponent
+    ProductsFormComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
